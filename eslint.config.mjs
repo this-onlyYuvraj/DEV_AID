441533bb3b0cf007533a35242397fe90f3c2422a
this-onlyYuvraj <this.is.onlyyuvraj@gmail.com>
2025-07-06 02:04:46 +0530

    again eslint rule change

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-empty-interface": "off"
     },
   }),
 ]
